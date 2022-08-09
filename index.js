67f480ff24ca61f1ae8323fc9ab8a65deae9d396
QuasimRizvi <quasi.rizvi@gmail.com>
2022-08-09 11:37:00 -0400

    ex03:done

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-var name = prompt("Enter your name")
+// var name = prompt("Enter your name")
 
-console.log(name)
-alert("Your name is "+name)
+// console.log(name)
+// alert("Your name is "+name)
+
+const btn = document.querySelector(".btn-primary")
+
+btn.addEventListener('click',_=>{
+    console.log("btn clicked")
+    setTimeout(() => {
+        alert("button-clicked")
+    }, 5000);
+})
