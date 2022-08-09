e8d53f7c340e389e0a194c2a94a93919308d25fa
QuasimRizvi <quasi.rizvi@gmail.com>
2022-08-09 18:12:52 -0400

    ex05:done; all bonus exercises completed

--- index.js
+++ index.js
@@ -5,15 +5,28 @@
 
 const btn = document.querySelector(".btn-primary")
 const tarea = document.querySelector("#input-text")
+const oarea = document.querySelector(".output")
 
-const input1 = document.querySelector("input[name=translator]")
-const allinput = document.querySelectorAll("input")
+// const input1 = document.querySelector("input[name=translator]")
+// const allinput = document.querySelectorAll("input")
 
-btn.addEventListener('click',_=>{
-    let inputtxt = tarea.value
-    console.log(inputtxt)
-    console.log(input1.value)
-    allinput.forEach(e=>{
-        console.log(e.value)
-    })
+// btn.addEventListener('click',_=>{
+//     let inputtxt = tarea.value
+//     console.log(inputtxt)
+//     console.log(input1.value)
+//     allinput.forEach(e=>{
+//         console.log(e.value)
+//     })
+// })
+
+btn.addEventListener("click",_=>{
+    let ip = tarea.value
+    console.log(ip)
+    //oarea.innerHTML = ip
+    oarea.innerHTML = ip
+    const op2 = document.createTextNode(ip+" This is using create Text Node")
+    document.querySelector("body").appendChild(op2)
+
+    const op3 = document.createTextNode(oarea.innerText+" This is using insert before")
+    document.body.insertBefore(op3,oarea)
 })
